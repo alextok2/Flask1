b5bbff9d644edb14f7b6b7b3408947aad45de1ce
alextok2 <alextok2@mail.ru>
2022-05-18 17:34:17 +0500

    Flask_site2

---
+++ Flask_site2.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8999, host='127.0.0.1') 
