e41044414a1c8496063b836e8735ff909cc193d2
alextok2 <alextok2@mail.ru>
2022-05-18 15:28:46 +0000

    User bd

--- Flask_site2.py
+++ Flask_site2.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, url_for, request
 
+from data import db_session
+from data.users import User
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_key'
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8999, host='127.0.0.1') 
+
+    db_session.global_init("db/blogs.sqlite")
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email
+    # app.run(debug=True, port=8999, host='127.0.0.1') 
